393b6a7b78ee56fadccf984ecc28b9fe6430d3a0
maxieffron <mleffron86@gmail.com>
2022-06-30 15:47:55 -0300

    Se agregan las metaetiquetas y los archivos necesarios para implementar SEO

--- index.php
+++ index.php
@@ -22,6 +22,20 @@ $reCaptcha = new ReCaptcha($secret);
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+        <meta name="google-site-verification" content="XMG1JerMtK1KqeYGRMduaVEXiVh0tlorh6pJVfdaIrs" />
+
+        <meta name="robots" content="index,follow">
+  
+        <meta name="description" content="Nacer con otro ser es un espacio para validar las emociones y poner en palabras lo que sentimos cuando nos atraviesa la maternidad.">
+
+        <meta name="keywords" content="Escritura terapéutica, Maternidad, Puerperio, Emociones, Crianza">
+
+        <meta name="author" content="Maximiliano Effron">
+  
+        <meta name="copyright" content="Daniela Effron">
+  
+        <link rel="canonical" href="https://www.nacerconotroser.com/">
+
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
             rel="stylesheet"
